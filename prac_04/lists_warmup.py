696608609980fe4b7ae8ecb39ecf3af0a55b6172
sheen <naturefernando.gonsalhewagesheen@my.jcu.edu.au>
2021-12-04 17:29:27 +0400

    Add list_warmup program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,18 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers [0] would give 3
+numbers[-1] would give 2
+numbers[3] would give 1
+numbers[:-1] removes 2
+numbers[3:4] returns 1
+5 in numbers returns true
+7 in numbers returns false
+"3" in numbers returns false
+numbers + [6, 5, 3] adds the values to list
+"""
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
