559d2479b14c8e8f5bdebfeb4b08404f1e89a2f4
Sheen <naturefernando.gonsalhewagesheen@my.jcu.edu.au>
2023-04-03 10:46:54 +0800

    Add list program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,36 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# what values do the following expressions have?
+# numbers[0]
+# first item in the list 3
+
+# numbers[-1]
+# last item in the list 2
+
+# numbers[3]
+# 4th item in the list 1
+
+# numbers[:-1])
+# every item in the list except for the last one [3, 1, 4, 1, 5]
+
+# numbers[3:4]
+# 4th item in the list 1
+
+# 5 in numbers
+# checks if 5 is an item in list (returns True)
+
+# 7 in numbers
+# checks if 7 is an item in list (returns False)
+
+# "3" in numbers
+# checks if "3" is an item in list (returns False)
+
+# numbers + [6, 5, 3]
+# adds 6, 5 and 3 to the end of list
+
+
+numbers.insert(0, "ten")
+numbers.append(1)
+print(numbers[2:])
+print(9 in numbers)
+
