4dacef4bcd1f4ab4dcdff831be53339e6c95b271
Sheen <naturefernando.gonsalhewagesheen@my.jcu.edu.au>
2023-04-10 10:35:37 +0800

    Reformat error handling

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,13 +10,18 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
-while state_code != "":
+try:
+    print(state_code, "is", CODE_TO_NAME[state_code])
+except KeyError:
+    print('invalid short state')
+    state_code = input("Enter short state: ").upper()
+
+"""while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ")"""
 
 for key in CODE_TO_NAME:
     print(f'{key} is {CODE_TO_NAME[key]}')
-
